522d856375ee3d8779a126b5569fc64925a11d06
Mark De Souza <markdesouza@Marks-iMac.local>
2017-09-04 15:42:21 -0700

    init

---
+++ wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Write your JavaScript code.
+function ThisFunction(id, person) {
+    
+}
+console.log('');
+// this function rocks
+
+// this function rocks
+
+function name(){
+    return value
+}
